9a73158cae87d93db9599017728e0494d3e712b0
Rodolfo E. Escobar U. <r.escurib@gmail.com>
2024-10-08 16:58:22 -0600

    refactor: style and comments

--- CursoSTM32/Lec_2/Practica_2_0/main.c
+++ CursoSTM32/Lec_2/Practica_2_0/main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  *  Created on: 9 jul 2023
- *  Author: Rodolfo Escobar (basado en ejemplo Carmine Noviello)
+ *  Author: Rodolfo Escobar (basado en ejemplo de Carmine Noviello)
  */
 
 #include "main.h"
@@ -26,13 +26,19 @@ uint32_t *vector_table[] __attribute__((section(".isr_vector"))) = {
 (uint32_t *)SRAM_END, // stack pointer inicial
 (uint32_t *)main      // función main como Reset_Handler
  };
+/*************************************************************/
 
-int main() {
-/* Habilitar reloj en GPIOC */
+void delay(uint32_t count) {
+ while(count--);
+}
+
+int main() 
+{
+ /* Habilitar reloj en GPIOC */
  RCC->APB2ENR |= (1<<4); // Setear bit 4: IOPCEN (RM0008, pag. 146)
 
-/* Configurar PC13 (LED) como salida push-pull */
- GPIOC->CRH |=  (1<<20); // MODE13[1:0]: Salida, vel. max. 10MHz (RM0008, pag. 172)
+ /* Configurar PC13 (LED) como salida push-pull */
+ GPIOC->CRH |=  (1<<20);    // MODE13[1:0]: Salida, vel. max. 10MHz (RM0008, pag. 172)
  GPIOC->CRH &= ~(0b11<<22); // CNF13[1:0]: Push-Pull (RM0008, pag. 172)
 
  while(1)
@@ -43,8 +49,4 @@ int main() {
   delay(200000);
  }
 
-}
-
-void delay(uint32_t count) {
- while(count--);
 }
